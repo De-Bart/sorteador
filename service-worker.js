6757ea6d7e8296792b8cf107abc6d4f7e0454c0a
De-Bart <debart1995@gmail.com>
2025-09-02 21:23:08 -0300

    Primeira Versão

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "sorteador-virtual-cache-v1";
+const FILES_TO_CACHE = [
+    "index.html",
+    "style.css",
+    "manifest.json",
+    "icons/icon-540.png",
+    "icons/logo.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(caches => {
+            return caches.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting()
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil (
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+    event.repondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
