c8ff3b4dfafca96bd2ecf8a8a8459842cc95e75b
De-Bart <debart1995@gmail.com>
2025-09-04 19:57:57 -0300

    Atualização 0.2

--- service-worker.js
+++ service-worker.js
@@ -7,22 +7,18 @@ const FILES_TO_CACHE = [
     "icons/logo.png"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(caches => {
-            return caches.addAll(FILES_TO_CACHE);
-        })
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
     );
-    self.skipWaiting()
+    self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-    event.waitUntil (
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-            );
-        })
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) =>
+            Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
+        )
     );
     self.clients.claim();
 });
